4f4d7a8ab58013455ee5e330f10b06fd96862a2a
Ari J <arijohand25@gmail.com>
2023-01-21 14:54:25 +0700

    add auth in prod router

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -5,8 +5,8 @@ const auth = require("../middlewares/auth");
 router.get("/product", product.getProduct);
 router.get("/product/:id", product.getById);
 router.get("/products/", product.getByName);
-router.post("/product", auth, product.addProduct);
-router.delete("/product/:id", product.deleteById);
-router.put("/product/:id", product.updateProduct);
+router.post("/product", auth, auth, product.addProduct);
+router.delete("/product/:id", auth, product.deleteById);
+router.put("/product/:id", auth, product.updateProduct);
 
 module.exports = router;
